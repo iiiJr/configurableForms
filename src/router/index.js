bac1ef2a37510a0b50a70fc855aa15511bba59b6
iiijr <908809979@qq.com>
2022-03-06 15:15:35 +0800

    :bug: delete静态页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import index from '@/views/index.vue'
+import index from '../views/index.vue'
 // import form from '@/views/form.vue'
 // import jsonEdit from '@/views/jsonEdit.vue'
 
