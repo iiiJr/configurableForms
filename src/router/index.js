3a8ee1bb7dd7486f03defd69535d4dcb97235eda
iiijr <908809979@qq.com>
2022-03-06 16:16:44 +0800

    :sparkles: 更新路径

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import index from '@'
+import Index from '@/views/index.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +8,7 @@ const routes = [
   {
     path: '/',
     name: 'index',
-    component: '../views/index.vue'
+    component: Index
   }
 ]
 
